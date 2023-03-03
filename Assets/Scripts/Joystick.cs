d4d470cd3e2d682e00e617de9707e9779d2258a6
wjdahdgus123@gmail.com <wjdahdgus123@gmail.com>
2023-03-03 21:48:27 +0900

    feat : 맵, 플레이어 구현

---
+++ Assets/Scripts/Joystick.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Joystick : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
